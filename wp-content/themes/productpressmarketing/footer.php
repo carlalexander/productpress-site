587e81e82842e8586ade038ec60a8764cbdaaf1c
Carl Alexander <carlalexander@gmail.com>
2020-12-29 15:51:51 -0500

    add updating date to footer

--- wp-content/themes/productpressmarketing/footer.php
+++ wp-content/themes/productpressmarketing/footer.php
@@ -8,17 +8,17 @@
                <ul>
                    <!-- Tooltips are optional! Leave the title attributes empty if you don't need tooltips! -->
                    <li><a href="https://www.facebook.com/pages/ProductPress/717206831734004" title="Like us on Facebook!" class="fb"><i class="fa fa-facebook"></i></a></li>
-                   <li><a href="https://twitter.com/productpresswp" title="Follow us on Twitter!" class="twitter"><i class="fa fa-twitter"></i></a></li>           
+                   <li><a href="https://twitter.com/productpresswp" title="Follow us on Twitter!" class="twitter"><i class="fa fa-twitter"></i></a></li>
                    <li><a href="https://www.youtube.com/channel/UCtK-FlvXXqQn2Aq0cPaT-gw/feed?view_as=public" title="Custom tool tip!" class="youtube"><i class="fa fa-youtube"></i></a></li>
                </ul>
           </div>
      <div class="clear"></div>
      </div>
-     
+
      <!-- COPYRIGHT -->
      <div id="copyright" class="wow fadeIn">
           <p>
-          ProductPress 2015 &copy; All Rights Reserved
+          ProductPress <?php echo date('Y'); ?> &copy; All Rights Reserved. Powered by <a href="https://ymirapp.com">Ymir</a>.
           </p>
      </div>
 
